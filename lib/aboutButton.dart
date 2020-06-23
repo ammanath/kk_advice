c32f590fd356f6a3314958fa002fa0a559eab7fd
bit00 <akbrijesh@gmail.com>
2020-06-23 20:13:51 +0530

    changes to about dialog

--- lib/aboutButton.dart
+++ lib/aboutButton.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutButton extends StatelessWidget {
   const AboutButton({
@@ -18,66 +19,49 @@ class AboutButton extends StatelessWidget {
   }
 
   void showAboutDialog(BuildContext context) {
-    var textAbout =
-        "Unsolicited app on KK's 68 bits of unsolicitied life advice \n";
-    String moreDetails = 'More Details: https://kk.org/thetechnium/68-bits-of-unsolicited-advice/ ';
-    String listen ='\nListen:';
-    String youTube =' https://www.youtube.com/watch?v=Zz70rcguxwk';
-    String fb = "\nFeedback:";
-    String email = " kkapp@ammanath.com";
-    String thanks =  "\nThanks for trying my app! ";
-    String signature = "\n\n- Brijesh";
+    String textAbout =
+        'Unsolicited app on KK\'s 68 bits of unsolicitied life advice \n';
+    String url =
+        'https://kk.org/thetechnium/68-bits-of-unsolicited-advice/ ';
+    String youTube = 'https://www.youtube.com/watch?v=Zz70rcguxwk';
+    String email = 'mailto:kkapp@ammanath.com?subject=Feedback_about_kkapp';
+    String thanks = '\nThanks for trying my app! ';
+    String signature = '\n\n- Brijesh';
     showDialog(
         context: context,
         barrierDismissible: true,
         builder: (context) {
           return AlertDialog(
-            title: Center(child: Text(
-              "About this app",
-              style: GoogleFonts.amarante(fontSize: 26, color: Colors.black54,)
-              )),
+            title: Center(
+                child: Text("About this app",
+                    style: GoogleFonts.amarante(
+                      fontSize: 26,
+                      color: Colors.black54,
+                    ))),
             content: Text.rich(
               TextSpan(
                 text: textAbout[0],
-                style: GoogleFonts.mali(
+                style: GoogleFonts.rumRaisin(
                   color: Colors.black54,
-                  letterSpacing: .5,
+                  //letterSpacing: .5,
                   fontSize: 22,
                 ),
                 children: [
                   TextSpan(
                     text: textAbout.substring(1),
-                    style: GoogleFonts.mali(fontSize: 14, color: Colors.black54),
+                    style:
+                        GoogleFonts.rumRaisin(fontSize: 14, color: Colors.black54),
                   ),
-                  TextSpan(
-                    text: moreDetails,
-                    style: GoogleFonts.mali(fontSize: 14, color: Colors.black54),
-                  ),
-                  TextSpan(
-                    text: fb,
-                    style: GoogleFonts.mali(fontSize: 12,  ),
-                  ),
-                  //TODO: Make email clickable
-                  TextSpan(
-                    text: email,
-                    style: GoogleFonts.shadowsIntoLight(fontSize: 14, color: Colors.blue ),
-                  ),
-                  TextSpan(
-                    text: listen,
-                    style: GoogleFonts.mali(fontSize: 12, ),
-                  ),
-                  //TODO: Show a youtube icon and make it clickable
-                  TextSpan(
-                    text: youTube,
-                    style: GoogleFonts.mali(fontSize: 12, color: Colors.pink ),
-                  ),
+
                   TextSpan(
                     text: thanks,
-                    style: GoogleFonts.mali(fontSize: 12, color: Colors.black54),
+                    style:
+                        GoogleFonts.rumRaisin(fontSize: 14, color: Colors.black54),
                   ),
                   TextSpan(
                     text: signature,
-                    style: GoogleFonts.dancingScript(fontSize: 20, color: Colors.blue),
+                    style: GoogleFonts.dancingScript(
+                        fontSize: 20, color: Colors.blue),
                   )
                 ],
               ),
@@ -85,8 +69,30 @@ class AboutButton extends StatelessWidget {
             elevation: 24.0,
             backgroundColor: Colors.blue[100],
             actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.open_in_browser),
+                color: Colors.red,
+                alignment: Alignment.bottomLeft,
+                onPressed: () => _launchURL(url),
+              ),
+              IconButton(
+                icon: Icon(Icons.feedback),
+                color: Colors.pink,
+                alignment: Alignment.bottomLeft,
+                onPressed: () => _launchURL(email),
+              ),
+              IconButton(
+                icon: Icon(Icons.videocam),
+                color: Colors.green,
+                alignment: Alignment.bottomLeft,
+                onPressed: () => _launchURL(youTube),
+              ),
               RaisedButton(
-                child: Text('Ok', style: GoogleFonts.amarante(fontSize: 26, color: Colors.black,)),
+                child: Text('Ok',
+                    style: GoogleFonts.amarante(
+                      fontSize: 26,
+                      color: Colors.black,
+                    )),
                 textColor: Colors.blue,
                 onPressed: () {
                   Navigator.of(context).pop();
@@ -96,5 +102,13 @@ class AboutButton extends StatelessWidget {
             ],
           );
         });
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
